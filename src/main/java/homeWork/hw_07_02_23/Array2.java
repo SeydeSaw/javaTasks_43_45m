7742b3115571d713f77fda727e4b719a1ee63ea3
SeydeSaw <115941526+SeydeSaw@users.noreply.github.com>
2023-02-08 15:34:37 +0100

    08.02.23

--- src/main/java/homeWork/hw_07_02_23/Array2.java
+++ src/main/java/homeWork/hw_07_02_23/Array2.java
@@ -11,14 +11,18 @@ package homeWork.hw_07_02_23;
  */
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Array2 {
 
     public static void main(String[] args){
-    //объявляем массив:
-    int[] array = new int[10];
-    //обращаемся к методу и передаём массив:
-    fillFibonacci(array);
+        //создаём сканнер для ввода с консоли:
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        //объявляем массив:
+        int[] array = new int[scanner.nextInt()];
+        //обращаемся к методу и передаём массив:
+        fillFibonacci(array);
     }
     public static void fillFibonacci(int[] array){
         // первые два элемента массива соответствуют начальным значениям
@@ -33,6 +37,7 @@ public class Array2 {
             array[i] = array[i-1] + array[i-2];
         }
         //Вывести массив на экран:
-        System.out.println(Arrays.toString(array));
+        System.out.println("Массив заполненный числами Фибоначчи: " + "\n" + (Arrays.toString(array)));
+        // \n  для перехода на следующую строку при выводе на экран
     }
 }
